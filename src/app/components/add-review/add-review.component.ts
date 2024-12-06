8c49c0da03eb5dc2c9b69aa71338a25e13abc4e0
brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>
2024-12-06 18:48:03 -0500

    feat: Star Rating Component

--- src/app/components/add-review/add-review.component.ts
+++ src/app/components/add-review/add-review.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { TextInputComponent } from '../uiComponents/text-input/text-input.component';
 import { CardComponent } from '../uiComponents/card/card.component';
+import { StarRatingComponent } from '../uiComponents/star-rating/star-rating.component';
 
 @Component({
   selector: 'brunch-add-review',
-  imports: [TextInputComponent, CardComponent],
+  imports: [TextInputComponent, CardComponent, StarRatingComponent],
   templateUrl: './add-review.component.html',
   styleUrl: './add-review.component.css'
 })
 export class AddReviewComponent {
 
   valueFromBrunchTextInput:string = '';
+
+  handleRating(rating:number) {
+    console.log('add review received: ' + rating);
+  }
 
 }
