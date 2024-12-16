ddb412032089d3717b6067999a2976a26e073b06
brunchinatorMaster <brunchinatorMaster@gmail.com>
2024-12-16 18:57:10 -0500

    Places autocomplete mvp (#9)
    * feat: Places Autocomplete MVP
    
    * cleanup
    
    ---------
    
    Co-authored-by: brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>

--- src/app/components/add-review/add-review.component.ts
+++ src/app/components/add-review/add-review.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TextInputComponent } from '../uiComponents/text-input/text-input.component';
 import { CardComponent } from '../uiComponents/card/card.component';
 import { StarRatingComponent } from '../uiComponents/star-rating/star-rating.component';
+import { PlacesAutocompleteComponent } from "../uiComponents/places-autocomplete/places-autocomplete.component";
 
 @Component({
   selector: 'brunch-add-review',
-  imports: [TextInputComponent, CardComponent, StarRatingComponent],
+  imports: [TextInputComponent, CardComponent, StarRatingComponent, PlacesAutocompleteComponent],
   templateUrl: './add-review.component.html',
   styleUrl: './add-review.component.css'
 })
@@ -15,6 +16,12 @@ export class AddReviewComponent {
 
   handleRating(rating:number) {
     console.log('add review received: ' + rating);
+  }
+
+  handlePlaceSelected(place: object) {//called when google places component emits location
+
+    console.log('handlePlaceSelected fires!');
+    console.log(place);
   }
 
 }
