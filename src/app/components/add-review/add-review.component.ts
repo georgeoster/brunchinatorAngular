9af5428f7e60b453719839c53057276f589c23fd
brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>
2024-12-16 18:52:40 -0500

    feat: Places Autocomplete MVP

--- src/app/components/add-review/add-review.component.ts
+++ src/app/components/add-review/add-review.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TextInputComponent } from '../uiComponents/text-input/text-input.component';
 import { CardComponent } from '../uiComponents/card/card.component';
 import { StarRatingComponent } from '../uiComponents/star-rating/star-rating.component';
+import { PlacesAutocompleteComponent } from "../uiComponents/places-autocomplete/places-autocomplete.component";
 
 @Component({
   selector: 'brunch-add-review',
-  imports: [TextInputComponent, CardComponent, StarRatingComponent],
+  imports: [TextInputComponent, CardComponent, StarRatingComponent, PlacesAutocompleteComponent],
   templateUrl: './add-review.component.html',
   styleUrl: './add-review.component.css'
 })
@@ -15,6 +16,34 @@ export class AddReviewComponent {
 
   handleRating(rating:number) {
     console.log('add review received: ' + rating);
+  }
+
+  getEstablishmentAddress(place: object) {//called when google places component emits location
+
+    console.log('getEstablishmentAddress fires!');
+    console.log(place);
+
+    // this.noPlace = false;
+    // this.placeErrorMessage = '';
+
+    // set map
+    // this.setMap(place['formatted_address'],15);
+
+    // this.brunchPlace = place['name'];
+    // this.placeId = place['place_id'];
+
+    // this.establishmentAddress = place['formatted_address'];
+    // this.phone = this.getPhone(place);
+    // this.formattedEstablishmentAddress = place['formatted_address'];
+    // this.zone.run(() => {
+    //   this.formattedEstablishmentAddress = place['formatted_address'];
+    //   this.phone = place['formatted_phone_number'];
+    // });
+
+    //this.image1 = place['photos'][0].getUrl({'maxWidth': 300, 'maxHeight': 300});
+    //this.image2 = place['photos'][1].getUrl({'maxWidth': 300, 'maxHeight': 300});
+    //this.image3 = place['photos'][2].getUrl({'maxWidth': 300, 'maxHeight': 300});
+   
   }
 
 }
---
+++ src/app/components/uiComponents/places-autocomplete/places-autocomplete.component.ts
@@ -0,0 +1,46 @@
+import { Component, ViewChild, EventEmitter, Output, OnInit, AfterViewInit, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf, NgClass } from '@angular/common';
+
+declare const google: any;
+
+type toEmit = {
+  place_id: string,
+}
+
+@Component({
+  selector: 'app-places-autocomplete',
+  imports: [FormsModule, NgIf, NgClass],
+  templateUrl: './places-autocomplete.component.html',
+  styleUrl: './places-autocomplete.component.css'
+})
+export class PlacesAutocompleteComponent {
+  @Input() hasError:boolean = false;
+  @Input() errorMessage:string = '';
+
+  @Output() setAddress: EventEmitter<toEmit> = new EventEmitter();
+  @ViewChild('placeInput') placeInput: any;
+
+  placeName: string = '';
+  inputAutocompleteWrapper: any;
+  autoCompleteOptions:{} =  {
+    componentRestrictions: { country: 'US' },
+    types: ['establishment'],
+    fields: ['place_id']
+  }
+
+  ngAfterViewInit() {
+    this.getPlaceAutocomplete();
+  }
+
+  private getPlaceAutocomplete() {
+    this.inputAutocompleteWrapper = new google.maps.places.Autocomplete(
+      this.placeInput.nativeElement,
+      this.autoCompleteOptions
+    );
+    this.inputAutocompleteWrapper.addListener('place_changed', () => {
+      const place = this.inputAutocompleteWrapper.getPlace();
+      this.setAddress.emit(place);
+    });
+  }
+}
