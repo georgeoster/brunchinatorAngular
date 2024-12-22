035c22dae8279e06eb1ea2099561a5de8e23a6e8
brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>
2024-12-22 16:33:55 -0500

    feat: Required 1 rating per review

--- src/app/components/uiComponents/star-rating/star-rating.component.ts
+++ src/app/components/uiComponents/star-rating/star-rating.component.ts
@@ -1,10 +1,10 @@
-import { Component, EventEmitter, Input, Output, } from '@angular/core';
-import { NgClass, NgFor } from '@angular/common';
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output, } from '@angular/core';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'brunch-star-rating',
-  imports: [NgClass, NgFor, FormsModule],
+  imports: [NgClass, NgFor, FormsModule, NgIf],
   templateUrl: './star-rating.component.html',
   styleUrl: './star-rating.component.css'
 })
@@ -12,10 +12,15 @@ export class StarRatingComponent {
 
   @Input() title:string = '';
   @Output() rating = new EventEmitter();
+  @Input() hasError:boolean = false;
+  errorMessage:string = 'At least one rating must be done.'
   selectedOption: number = 0;
   options = [1,2,3,4,5];
 
+  constructor(private changeDetectorRef: ChangeDetectorRef){}
+
   onOptionChange() {
+    this.changeDetectorRef.detectChanges();
     this.rating.emit(this.selectedOption);
   }
 }
--- src/app/utils/types/all.types.ts
+++ src/app/utils/types/all.types.ts
@@ -52,8 +52,8 @@ export type Review = {
   placeId: string,
   userName: string,
   placeName: string,
-  burger: number,
-  bloody: number,
+  burger: number | null,
+  bloody: number | null,
   words: string,
   reviewDate: string,
 }
