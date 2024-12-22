eb31e87a3c2d39c0fc17d3140780bf061d6630d4
brunchinatorMaster <brunchinatorMaster@gmail.com>
2024-12-22 16:34:20 -0500

    feat: Required 1 rating per review (#15)
    Co-authored-by: brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>

--- src/app/components/add-review/review-place/review-place.component.ts
+++ src/app/components/add-review/review-place/review-place.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
 import { autoCompletePlace, Review, User } from '../../../utils/types/all.types';
 import { StarRatingComponent } from '../../uiComponents/star-rating/star-rating.component';
 import { CardComponent } from '../../uiComponents/card/card.component';
@@ -7,10 +7,11 @@ import { UserService } from '../../../services/user.service';
 import { Subscription } from 'rxjs';
 import { FormsModule } from '@angular/forms';
 import { AddReviewService } from '../../../services/add-review.service';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-review-place',
-  imports: [CardComponent, StarRatingComponent, ButtonComponent, FormsModule],
+  imports: [CardComponent, StarRatingComponent, ButtonComponent, FormsModule, NgFor, NgIf, NgClass],
   templateUrl: './review-place.component.html',
   styleUrl: './review-place.component.css'
 })
@@ -22,20 +23,26 @@ export class ReviewPlaceComponent {
     placeId: '',
     userName: '',
     placeName: '',
-    burger: 0,
-    bloody: 0,
+    burger: null,
+    bloody: null,
     words: '',
     reviewDate: new Date().toLocaleDateString(),
   }
   userSubscription: Subscription = new Subscription;
+  noReviewError:boolean = false;
 
-  constructor(private userService: UserService, private addReviewService: AddReviewService) {
+  stars = [
+    { title: 'Burger', field: 'burger' },
+    { title: 'Bloody', field: 'bloody' }
+  ]
+
+  constructor(private userService: UserService, private addReviewService: AddReviewService, private changeDetectorRef: ChangeDetectorRef) {
     this.subscribeToUser();
   }
 
   subscribeToUser(){
     this.userSubscription = this.userService.user.subscribe((user: User) => {
-      this.review.userName = user.userName;
+      this.review.userName = user?.userName;
     });
   }
 
@@ -49,12 +56,31 @@ export class ReviewPlaceComponent {
     this.review.placeName = this.place?.name;
   }
 
+  formIsInvalid() {
+    return this.review.burger == null && this.review.bloody == null;
+  }
+
+  displayErrors() {
+    this.noReviewError = true;
+    this.changeDetectorRef.detectChanges();
+  }
+
   handleRating(e:number, field:string) {
     if(field === 'burger') this.review.burger = e;
     if(field === 'bloody') this.review.bloody = e;
+    this.noReviewError = false;
+    if (this.formIsInvalid()) {
+      this.displayErrors();
+      return;
+    }
   }
 
   handlePostReview(){
+    this.noReviewError = false;
+    if (this.formIsInvalid()) {
+      this.displayErrors();
+      return;
+    }
     this.addReviewService.addReview(this.review);
   }
 }
