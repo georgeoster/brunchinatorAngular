a5ce664f2e56fd0d1ba77ddb7395361783bd084b
brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>
2024-12-22 13:44:46 -0500

    chore: fixed broken tests

--- src/app/components/add-review/review-place/review-place.component.spec.ts
+++ src/app/components/add-review/review-place/review-place.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
 import { ReviewPlaceComponent } from './review-place.component';
+import { AddReviewService } from '../../../services/add-review.service';
 
 describe('ReviewPlaceComponent', () => {
   let component: ReviewPlaceComponent;
@@ -8,11 +9,22 @@ describe('ReviewPlaceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReviewPlaceComponent]
+      imports: [ReviewPlaceComponent],
+      providers: [
+        AddReviewService,
+        provideHttpClient()
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ReviewPlaceComponent);
+    fixture.componentInstance.place = {
+      place_id: '12345',
+      name: 'test place',
+      vicinity: '123 main st',
+      formatted_phone_number: '(123) 456-7890',
+      photos: []
+    }
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
