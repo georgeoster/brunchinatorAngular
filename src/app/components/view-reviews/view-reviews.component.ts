2e4665aaa7d6dc796e0ce4aaa7ec3b7ad154c426
brunchinatorMaster <brunchinatorMaster@gmail.com>
2024-12-28 16:30:52 -0500

    feat: Place Card (#19)
    Co-authored-by: brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>

--- src/app/components/view-reviews/view-reviews.component.ts
+++ src/app/components/view-reviews/view-reviews.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PlaceCardComponent } from '../uiComponents/place-card/place-card.component';
 
 @Component({
   selector: 'brunch-view-reviews',
-  imports: [],
+  imports: [PlaceCardComponent],
   templateUrl: './view-reviews.component.html',
   styleUrl: './view-reviews.component.css'
 })
