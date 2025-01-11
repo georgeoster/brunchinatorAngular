86242d4de990df157ef865180be2917494af6437
brunchinatorMaster <brunchinatorMaster@gmail.com>
2025-01-11 16:53:27 -0500

    feat: upload profile picture (#21)
    Co-authored-by: brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { UploadImageService } from '../../services/upload-image.service';
+import { s3Host } from '../../utils/http/consts';
+import { UserService } from '../../services/user.service';
+import { Subscription } from 'rxjs';
+import { User } from '../../utils/types/all.types';
 
 @Component({
   selector: 'brunch-profile',
@@ -8,4 +13,48 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  imageToUpload!:File;
+  user!:User;
+  profilePicture!:String;
+  userServiceSubscription:Subscription = new Subscription();
+  uploadImageServiceSubscription:Subscription = new Subscription();
+
+  constructor(private uploadImageService: UploadImageService, private userService: UserService, private changeDetectorRef: ChangeDetectorRef){
+    this.subscribeToUserService();
+    this.subscribeToUploadImageService();
+  }
+
+  subscribeToUserService(){
+    this.userServiceSubscription = this.userService.user.subscribe((user: User) => {
+      this.user = user;
+      this.profilePicture = `${s3Host}/${user.userName}`;
+    });
+  }
+
+  subscribeToUploadImageService(){
+    this.uploadImageServiceSubscription = this.uploadImageService.uploadImageResponse.subscribe((response) => {
+      //TODO force re-getting of image
+      this.profilePicture = '';
+      this.changeDetectorRef.detectChanges();
+      this.profilePicture = `${s3Host}/${this.user.userName}`;
+      this.changeDetectorRef.detectChanges();
+    });
+  }
+
+  ngOnDestroy() {
+    this.userServiceSubscription.unsubscribe();
+    this.uploadImageServiceSubscription.unsubscribe();
+  }
+
+  updateProfilePicture(){
+    const toUpload = new FormData();
+    toUpload.append('image', this.imageToUpload);
+    toUpload.append('userName', this.user.userName);
+    this.uploadImageService.uploadImage(toUpload);
+  }
+
+  imageUploadHandler(e:any) {
+    this.imageToUpload = e.target.files[0];
+    this.updateProfilePicture();
+  }
 }
---
+++ src/app/services/upload-image.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Subject, throwError } from 'rxjs';
+import { host } from '../utils/http/consts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadImageService {
+
+  private uploadImageResponseSubject = new Subject();
+  uploadImageResponse = this.uploadImageResponseSubject.asObservable();
+
+  constructor(private http:HttpClient) { }
+
+  uploadImage(body:any) {
+    this.http.post(`${host}/users/userProfilePicture`, body)
+    .pipe(catchError(
+      (error: HttpErrorResponse) => {
+        const serviceError = {
+          statusCode: error.status,
+          message: error.error.message,
+        }
+        this.uploadImageResponseSubject.next(serviceError);
+        return throwError(() => new Error('something went wrong. please try again later.'));
+      }
+    ))
+    .subscribe((response:any) => {
+      this.uploadImageResponseSubject.next(response);
+    })
+  }
+
+
+}
