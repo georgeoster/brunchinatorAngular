796d8078bd8d22500c1d89b58e00860e132c82f8
brunchinatorMaster <brunchinatorMaster@gmail.com>
2025-01-18 13:21:14 -0500

    feat: place page MVP (#26)
    Co-authored-by: brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>

--- src/app/components/place/place.component.ts
+++ src/app/components/place/place.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { Place } from '../../utils/types/all.types';
+import { getImageFromPlaceId } from '../../utils/placeUtils';
+import { PlaceReviewsComponent } from './place-reviews/place-reviews.component';
 
 @Component({
   selector: 'brunch-place',
-  imports: [],
+  imports: [PlaceReviewsComponent],
   templateUrl: './place.component.html',
   styleUrl: './place.component.css'
 })
 export class PlaceComponent {
+  @Input() place: Place = {
+    placeId: 'ChIJOaE5coXIxokRpQt1kuCvTZs',
+    placeName: 'Royal Boucherie',
+    bloody: 4,
+    burger: 5,
+    numberOfReviews: 1,
+    overallRating: 4.5
+   };
+  mainImageSrc:string = '';
 
+  ngOnInit(){
+    this.populateMainImageSrc()
+  }
+
+  async populateMainImageSrc() {
+    this.mainImageSrc =  await getImageFromPlaceId(this.place.placeId);
+  }
 }
