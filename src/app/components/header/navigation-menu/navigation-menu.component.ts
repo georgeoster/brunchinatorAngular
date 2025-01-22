bec10530f5354f93d0a7fd1ecdf7dc1354f955e2
brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>
2025-01-22 17:58:49 -0500

    feat: dynamic routes

--- src/app/components/header/navigation-menu/navigation-menu.component.ts
+++ src/app/components/header/navigation-menu/navigation-menu.component.ts
@@ -3,8 +3,9 @@ import { NgClass, NgFor } from '@angular/common';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { MenuService } from '../../../services/menu.service';
-import { NavigationMenuItem } from '../../../utils/types/all.types';
+import { NavigationMenuItem, User } from '../../../utils/types/all.types';
 import { ROUTE_NAMES } from '../../../utils/types/globalsConsts';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'brunch-navigation-menu',
@@ -17,22 +18,34 @@ export class NavigationMenuComponent {
   showMenu:boolean = false;
   menuItems:Array<NavigationMenuItem> = [
     {icon: 'home', label: 'Home', route: ROUTE_NAMES.HOME},
-    {icon: 'home', label: 'Add Review', route: ROUTE_NAMES.ADD_REVIEW},
     {icon: 'home', label: 'View Reviews', route: ROUTE_NAMES.VIEW_REVIEWS },
-    {icon: 'home', label: 'Profile', route: ROUTE_NAMES.PROFILE },
-    {icon: 'home', label: 'Sign Up', route: ROUTE_NAMES.SIGN_UP },
+    {icon: 'home', label: 'Register', route: ROUTE_NAMES.SIGN_UP },
     {icon: 'home', label: 'Sign In', route: ROUTE_NAMES.SIGN_IN },
   ];
   menuServiceSubscription:Subscription = new Subscription;
-  loginServiceSubscription:Subscription = new Subscription;
+  userServiceSubscription:Subscription = new Subscription;
 
-  constructor(private menuService:MenuService, private router: Router){
+  constructor(private menuService:MenuService, private userService:UserService, private router: Router){
     this.subscribeToMenuService();
+    this.subscribeToUserService();
   }
 
   ngOnDestroy() {
     this.menuServiceSubscription.unsubscribe();
-    this.loginServiceSubscription.unsubscribe();
+    this.userServiceSubscription.unsubscribe();
+  }
+
+  subscribeToUserService(){
+    this.userServiceSubscription = this.userService.user.subscribe((user: User) => {
+      if(user?.token?.length > 0) {
+        this.menuItems = [
+          {icon: 'home', label: 'Home', route: ROUTE_NAMES.HOME},
+          {icon: 'home', label: 'Add Review', route: ROUTE_NAMES.ADD_REVIEW},
+          {icon: 'home', label: 'View Reviews', route: ROUTE_NAMES.VIEW_REVIEWS },
+          {icon: 'home', label: 'Profile', route: `${ROUTE_NAMES.PROFILE}/${user.userName}` },
+        ];
+      }
+    });
   }
 
   subscribeToMenuService(){
