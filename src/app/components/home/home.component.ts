11a983ab00a19413f15e30191a1499e492971f04
brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>
2025-02-05 11:28:42 -0500

    chore: copy change on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../uiComponents/card/card.component';
+import { Router } from '@angular/router';
+import { ROUTE_NAMES } from '../../utils/types/globalsConsts';
 
 @Component({
   selector: 'brunch-home',
@@ -8,5 +10,13 @@ import { CardComponent } from '../uiComponents/card/card.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  ROUTE_NAMES = ROUTE_NAMES;
+
+  constructor(private router:Router){}
+
+  routeTo(route:string){
+    this.router.navigate([route])
+  }
 
 }
