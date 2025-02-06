abf97d60fcb3fde4bdd94885446e70e44d0c4510
brunchinatorMaster <brunchinatorMaster@gmail.com>
2025-02-06 16:53:51 -0500

    feat:UI Enchancements (#63)
    Co-authored-by: brunchinatorMaster <georgeoster@georges-air-2.myfiosgateway.com>

--- src/app/utils/http/consts.ts
+++ src/app/utils/http/consts.ts
@@ -1,7 +1,7 @@
 // export const host:string = 'https://brunchinatorbeanstalk-env.eba-i3r2rbwp.us-east-2.elasticbeanstalk.com';
 export const host:string = 'https://osteroush.com/brunchinatorBackend';
 
-// export const host:string = 'http://localhost:3000';
+// export const host:string = 'http://localhost:3000/brunchinatorBackend';
 
 export const s3Host:string = 'https://brunchinator-user-profile-pictures.s3.us-east-2.amazonaws.com';
 
