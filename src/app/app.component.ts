1910c413bb361fe05b83251c750d241ea32c81cb
georgeoster <georgeostercodes@gmail.com>
2025-05-27 18:09:23 -0400

    redesign: Sign In

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header//header/header.component';
-import { NavigationMenuComponent } from "./components/header/navigation-menu/navigation-menu.component";
+import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NavigationMenuComponent],
+  imports: [LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
