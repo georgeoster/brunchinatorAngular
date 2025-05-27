476e42b4a9d4df2263b472a9674d7aeb91488578
georgeoster <georgeostercodes@gmail.com>
2025-05-27 16:54:36 -0400

    chore: Install Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // scan component HTML + TS templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
