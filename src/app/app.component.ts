3c14674d52dc9aa804e182958c1f5a74f055a707
george oster <georgeostercodes@gmail.com>
2025-05-31 14:34:33 -0400

    feat: Google Analytics (#90)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   imports: [LayoutComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-DGZK35L77T', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
+
   title = 'brunchinatorAngular';
 }
